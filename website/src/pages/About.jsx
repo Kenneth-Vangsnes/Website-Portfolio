d9d0e07423c7843b57b5abc72240912518ee7770
Arfaleas <vangsnesk@gmail.com>
2023-09-06 12:55:52 +0200

    Initial commit

---
+++ website/src/pages/About.jsx
@@ -0,0 +1,95 @@
+import styled from "styled-components"
+import ProfilePicture from "../assets/picture.jpg"
+
+const Section = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background-color: #4e4eff;
+`
+
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  @media only screen and (max-width: 768px) {
+    flex-direction: column;
+  }
+`
+
+const Left = styled.div`
+  height: 100vh;
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: right;
+
+  @media only screen and (max-width: 768px) {
+    align-items: flex-end;
+  }
+`
+const Picture = styled.img`
+  height: 200px;
+  width: 200px;
+  object-fit: cover;
+  border-radius: 50%;
+  margin-right: 25px;
+
+  @media only screen and (max-width: 768px) {
+    align-items: center;
+    margin-right: 0;
+    justify-content: flex-end;
+    margin-bottom: 25px;
+  }
+`
+const Right = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: left;
+  flex: 1;
+
+  @media only screen and (max-width: 768px) {
+    align-items: flex-start;
+    margin-top: 2%;
+  }
+`
+
+const Text = styled.div`
+  display: flex;
+  max-width: 250px;
+  font-size: 16px;
+  margin-left: 25px;
+
+  @media only screen and (max-width: 768px) {
+    text-align: center;
+    margin-left: 0;
+  }
+`
+
+const About = () => {
+  return (
+    <Section id="About">
+      <Container>
+        <Left>
+          <Picture src={ProfilePicture} />{" "}
+        </Left>
+        <Right>
+          <Text>
+            Another web developer portfolio worth mentioning is Adham
+            Dannaway’s. The homepage features an eye-catching graphic that
+            demonstrates his coding and design skills. Despite its simple
+            appearance, the portfolio displays elegance and meticulous attention
+            to detail.
+          </Text>
+        </Right>
+      </Container>
+    </Section>
+  )
+}
+
+export default About
