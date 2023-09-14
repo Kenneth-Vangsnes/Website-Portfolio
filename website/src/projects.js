e3c4ed682f83b046628e20f4be5c584a437aed3d
Arfaleas <vangsnesk@gmail.com>
2023-09-14 12:26:20 +0200

    Added project, fixed picture links

--- website/src/projects.js
+++ website/src/projects.js
@@ -2,15 +2,22 @@ export const projects = [
   {
     id: 1,
     name: "Website Restaurant",
-    image: "public/preview-website-restaurant.jpg",
+    image: "https://images2.imgbox.com/e5/1c/6CPNjmBj_o.jpg",
     demoURL: "https://restaurant-website-pi-inky.vercel.app/",
     github: "https://github.com/Kenneth-Vangsnes/website",
   },
   {
     id: 2,
     name: "Hangman",
-    image: "public/preview-hangman.jpg",
+    image: "https://images2.imgbox.com/ef/ec/OyxDPxmN_o.jpg",
     demoURL: "https://hangman-one-beta.vercel.app/",
     github: "https://github.com/Kenneth-Vangsnes/hangman",
+  },
+  {
+    id: 2,
+    name: "Weather site",
+    image: "",
+    demoURL: "https://test-kappa-snowy.vercel.app/",
+    github: "https://github.com/Kenneth-Vangsnes/test",
   },
 ]
